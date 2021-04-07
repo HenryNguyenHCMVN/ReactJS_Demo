9de9580a3380939ac00993bcfe17c6f9ed6444a1
HenryNguyenHCMVN <huyhumor@gmail.com>
2021-04-07 20:41:36 +0700

    upload

--- src/index.js
+++ src/index.js
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import "../node_modules/jquery/dist/jquery";
+import "../node_modules/popper.js/dist/popper";
+import "../node_modules/bootstrap/dist/js/bootstrap";
 
 ReactDOM.render(
   <React.StrictMode>
